04e31c33bff8a6884846e0a95ec4f610e4529abb
ObscureAngel <Obscure-Angel@gmx.fr>
2019-04-01 14:28:25 +0200

    Création des murs

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame, sys
+import pygame, sys, math
 from pygame.locals import *
 from classes import *
 
 BLACK = (0, 0, 0)
 RED = (125, 0, 0)
+BLUE = (0, 125, 255)
 
 pygame.init()
 root = pygame.display.set_mode((400, 600))
@@ -13,11 +14,16 @@ theo = Brick(0, 0, 20, 20, RED, 5)
 
 done = False
 while not done:
+    # definition du fond et des contours
     root.fill(BLACK)
+    Brick(0, 0, 10, 600, BLUE, math.inf).draw(root)
+    Brick(390, 0, 10, 600, BLUE, math.inf).draw(root)
+    Brick(0, 0, 400, 10, BLUE, math.inf).draw(root)
+
     for event in pygame.event.get():
         if event.type is QUIT: done = True
         if event.type is KEYDOWN:
-            theo.x += 5
+            theo.pos.x += 5
 
     theo.draw(root)
     pygame.display.flip()
