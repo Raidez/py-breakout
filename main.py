91254d0f1c9f30147be7f3085ff45a02e13efef8
ObscureAngel <Obscure-Angel@gmx.fr>
2019-04-01 14:37:56 +0200

    Correction allocation mémoire

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ BLUE = (0, 0, 125)
 pygame.init()
 root = pygame.display.set_mode((400, 600))
 
+# definition des contours
+mur_gauche = Brick(0, 0, 10, 600, BLUE, math.inf)
+mur_droit = Brick(390, 0, 10, 600, BLUE, math.inf)
+mur_haut = Brick(0, 0, 400, 10, BLUE, math.inf)
+
 # definition des objets
 brick = Brick(0, 0, 20, 20, RED, 5)
 pad = Pad(150, 575, 100, 15, WHITE)
 
 done = False
 while not done:
-    # definition du fond et des contours
+    # mise a jour du fond et des contours
     root.fill(BLACK)
-    Brick(0, 0, 10, 600, BLUE, math.inf).draw(root)
-    Brick(390, 0, 10, 600, BLUE, math.inf).draw(root)
-    Brick(0, 0, 400, 10, BLUE, math.inf).draw(root)
+    mur_haut.draw(root)
+    mur_droit.draw(root)
+    mur_gauche.draw(root)
 
     for event in pygame.event.get():
         if event.type == QUIT: done = True
