e9e42d8d63c715afa1fd5296bc7af8e4379dfe7f
Raidez <alexis.lucien@outlook.fr>
2019-04-02 12:01:19 +0200

    Refonte du système de scène

--- main.py
+++ main.py
@@ -1,103 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pygame, sys, math
+import pygame
 from pygame.locals import *
 from utils import *
+from scenes import *
 
 pygame.init()
 root = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.key.set_repeat(10, 10)
 
-# Affichage texte Win
-win = pygame.Surface((WIDTH, HEIGHT))
-pygame.display.set_caption('Winner')
-font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Winner', True, BLACK, WHITE)
-textRect = text.get_rect()
-textRect.center = (WIDTH // 2, HEIGHT // 2)
-
-win.fill(WHITE)
-win.blit(text, textRect)
-
-no_bricks = False
+# configuration des scènes
+game = GameScene(WIDTH, HEIGHT)
+win = WinScene(WIDTH, HEIGHT)
+current = game
 done = False
 
 # définition des objets
 clock = pygame.time.Clock()
-left_wall = Brick(0, 0, 10, HEIGHT, math.inf)
-right_wall = Brick(WIDTH-10, 0, 10, HEIGHT, math.inf)
-top_wall = Brick(0, 0, WIDTH, 10, math.inf)
-pad = Pad(150, 575, 100, 15, WHITE)
-ball = Ball(root.get_width() // 2, root.get_height() // 2, 10, WHITE, vy=-1.2, vx=-1.8)
-
-bricks = []
-number_row = 2
-for i in range(1, number_row):
-    # remplissage des briques
-    height = 30 * i
-    bricks.append(Brick(30, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(80, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(130, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(180, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(230, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(280, height, 40, 20, number_row + 1 -i))
-    # bricks.append(Brick(330, height, 40, 20, number_row + 1 -i))
-
-def update():
-    global done, ball, bricks, no_bricks
-
-    # gestion des événements (bouger la barre, quitter l'appli)
-    for event in pygame.event.get():
-        if event.type == QUIT: done = True
-        if event.type == KEYDOWN:
-            if event.key == K_RIGHT and pad.x < right_wall.x - pad.w:
-                pad.x += 5
-            if event.key == K_LEFT and pad.x > left_wall.w:
-                pad.x -= 5
-            if event.key == K_UP:
-                ball.speed += 0.2
-            if event.key == K_DOWN:
-                ball.speed -= 0.2
-        if event.type == KEYUP and event.key == K_r:
-            ball = Ball(root.get_width() // 2, root.get_height() // 2, 10, WHITE, vy=-1.2, vx=-1.8)
-
-    # gestion du mouvement de la balle
-    ball.update(left_wall, top_wall, right_wall, pad)
-
-    # on enlève 1 HP à chaque touche
-    for index in ball.hitbox.collidelistall(bricks):
-        ball.rebound_vertical()
-        brick = bricks[index]
-        brick.hp -= 1
-
-        if brick.hp <= 0:
-            del bricks[index]
-
-    if len(bricks) == 0:
-        no_bricks = True
-
-
-def draw():
-    global bricks, no_bricks
-    top_wall.draw(root)
-    right_wall.draw(root)
-    left_wall.draw(root)
-
-    pad.draw(root)
-    ball.draw(root)
-
-    for brick in bricks:
-        brick.draw(root)
-
-    if no_bricks == True:
-        root.blit(win, (0,0))
 
 while not done:
-    root.fill(BLACK)
-    update()
-    draw()
-    pygame.display.update()
-    clock.tick(60) # permet de bloquer le framerate à 60 images par secondes
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT: done = True
+
+    if not done:
+        # s'il n'y a plus de briques alors victoire
+        if len(game.bricks) == 0:
+            current = win
+
+        done = current.update(events)
+        current.draw()
+        root.blit(current, (0,0))
+
+        pygame.display.update()
+        clock.tick(60) # permet de bloquer le framerate à 60 images par secondes
 
 pygame.quit()
---
+++ scenes.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pygame, math
+from utils import *
+from pygame.locals import *
+
+class Scene(pygame.Surface):
+    def __init__(self, width, height):
+        super().__init__((width, height))
+
+    def update(self, events):
+        return False
+
+    def draw(self):
+        pass
+
+# initialisation de la scène de jeu
+class GameScene(Scene):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        self.left_wall = Brick(0, 0, 10, height, math.inf)
+        self.right_wall = Brick(width-10, 0, 10, height, math.inf)
+        self.top_wall = Brick(0, 0, width, 10, math.inf)
+
+        self.pad = Pad(150, 575, 100, 15, WHITE)
+        self.ball = Ball(width // 2, height // 2, 10, WHITE, vy=-1.2, vx=-1.8)
+
+        self.bricks = []
+        number_row = 2
+        for i in range(1, number_row):
+            # remplissage des briques
+            height = 30 * i
+            self.bricks.append(Brick(30, height, 40, 20, number_row + 1 -i))
+
+    def update(self, events):
+        # gestion des événements (bouger la barre, quitter l'appli)
+        for event in events:
+            if event.type == KEYDOWN:
+                if event.key == K_RIGHT and self.pad.x < self.right_wall.x - self.pad.w:
+                    self.pad.x += 5
+                if event.key == K_LEFT and self.pad.x > self.left_wall.w:
+                    self.pad.x -= 5
+                if event.key == K_UP:
+                    self.ball.speed += 0.2
+                if event.key == K_DOWN:
+                    self.ball.speed -= 0.2
+            if event.type == KEYUP and event.key == K_r:
+                self.ball = Ball(self.get_width() // 2, self.get_height() // 2, 10, WHITE, vy=-1.2, vx=-1.8)
+
+        # gestion du mouvement de la balle
+        self.ball.update(self.left_wall, self.top_wall, self.right_wall, self.pad)
+
+        # on enlève 1 HP à chaque touche
+        for index in self.ball.hitbox.collidelistall(self.bricks):
+            self.ball.rebound_vertical()
+            brick = self.bricks[index]
+            brick.hp -= 1
+
+            if brick.hp <= 0:
+                del self.bricks[index]
+
+        return False
+
+    def draw(self):
+        self.fill(BLACK)
+
+        self.top_wall.draw(self)
+        self.right_wall.draw(self)
+        self.left_wall.draw(self)
+
+        self.pad.draw(self)
+        self.ball.draw(self)
+
+        for brick in self.bricks:
+            brick.draw(self)
+
+
+# initialisation de la scène de victoire
+class WinScene(Scene):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        self.text = font.render('Winner', True, BLACK, WHITE)
+
+    def draw(self):
+        self.fill(WHITE)
+        text_rect = self.text.get_rect()
+        text_rect.center = (self.get_width() // 2, self.get_height() // 2)
+        self.blit(self.text, text_rect)
